8514955ef0e75d8d9e985b09a132b2ebd82ac6e7
Gabriel Kunkel <gabriel.kunkel@syndigo.com>
2021-06-30 23:54:27 -0500

    production builds and without dev tools

--- src/electron.js
+++ src/electron.js
@@ -7,6 +7,9 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
+const args = process.argv.slice(1);
+const isDev = args.some(x => x === '--dev');
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
@@ -25,7 +28,11 @@ function createWindow() {
     mainWindow.loadURL(startUrl);
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    if(isDev)
+        mainWindow.webContents.openDevTools();
+
+        
+    console.log(process.env);
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
